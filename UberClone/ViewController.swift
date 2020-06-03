5eed04806bab4a0bb1fa1992fef0576e81919a20
Fellipe Ricciardi Chiarello <fellipe.chiarello@gmail.com>
2020-06-02 21:36:43 -0300

    first commit

---
+++ UberClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UberClone
+//
+//  Created by Fellipe Ricciardi Chiarello on 6/2/20.
+//  Copyright © 2020 fchiarello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
