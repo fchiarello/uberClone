15e0d00ae077ec6c6386bf545a6c7961a907a7dc
Fellipe Ricciardi Chiarello <fellipe.chiarello@gmail.com>
2020-07-14 20:34:56 -0300

    Cria Controller de Passageiros

--- UberClone/Views/LoginViewController.swift
+++ UberClone/Views/LoginViewController.swift
@@ -7,10 +7,27 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController, Storyboarded {
     
     var coordinator: LoginCoordinator?
+    
+    //MARK: - Outlets / Actions
+
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBAction func loginButton(_ sender: Any) {
+        let emptyField = self.checkForEmptyFields()
+        
+        if emptyField == "" {
+            self.authUser()
+        }else {
+            print("O campo \(emptyField), não foi preenchido!")
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +39,34 @@ class LoginViewController: UIViewController, Storyboarded {
         self.view.endEditing(true)
     }
     
+    func checkForEmptyFields() -> String {
+        if (self.email.text?.isEmpty)! {
+            return "e-mail"
+        } else if (self.password.text?.isEmpty)! {
+            return "senha"
+        }
+        return ""
+    }
+    
+    func authUser() {
+        let auth = Auth.auth()
+        
+        if let email = self.email.text{
+            if let password = self.password.text{
+                auth.signIn(withEmail: email, password: password) { (user, error) in
+                    if error == nil {
+                        if user != nil {
+                            print("Sucesso ao logar")
+                        }
+                    }else{
+                        print("Erro ao logar")
+                    }
+                }
+            }
+        }
+        
+        
+    }
 //    override func viewDidDisappear(_ animated: Bool) {
 //        super.viewDidDisappear(animated)
 //        
